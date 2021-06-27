e2c9c30876d866ecdd53d19f453cf69de15d0dc1
Esteban1891 <esteban.delahoz15@gmail.com>
2021-06-26 20:17:15 -0500

    [add features to admin dashboard]

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
-
+from django import forms
 from .models import Language
 
 
+class LanguageForm(forms.ModelForm):
+    class Meta:
+        model = Language
+        exclude = ['slug']
+
 @admin.register(Language)
 class LanguageAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('name', 'slug')
+    form = LanguageForm
